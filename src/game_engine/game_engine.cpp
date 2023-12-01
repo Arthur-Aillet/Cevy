820477c1b0b7f91ff0f10f431de8d7084a9fddc4
Arthur-Aillet <arthur.aillet@epitech.eu>
2023-12-01 22:58:29 +0100

    feat(Engine): togglable debug window

--- src/game_engine/game_engine.cpp
+++ src/game_engine/game_engine.cpp
@@ -11,32 +11,62 @@ Cevy::GameEngine::GameEngine() {
     rlImGuiSetup(true);
 }
 
-void Cevy::GameEngine::update(void) {
-    Cevy::Camera camera;
+void Cevy::GameEngine::DebugWindow (void) {
+    if (IsKeyPressed(KEY_F10)) {
+        debugMode = (debugMode == false);
+        if (debugMode)
+            EnableCursor();
+    }
+    if(debugMode) {
+        rlImGuiBegin();
+        if(!ImGui::Begin("Test WIndow", &debugMode)) {
+            ImGui::End();
+        } else {
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                        1000.0f / ImGui::GetIO().Framerate,
+                        ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+        rlImGuiEnd();
+    } else {
+        UpdateCamera(camera, CAMERA_FIRST_PERSON);
+        DisableCursor();
+    }
+}
 
-    rlImGuiSetup(true);
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        UpdateCamera(camera, CAMERA_FREE);
+void Cevy::GameEngine::Fullscreen (void) {
+    if (IsWindowFullscreen()) {
+        int display = GetCurrentMonitor();
+        this->screenWidth = GetMonitorWidth(display);
+        this->screenHeight = GetMonitorHeight(display);
+    } else {
         this->screenWidth = GetScreenWidth();
         this->screenHeight = GetScreenHeight();
+    }
+    if (!IsKeyPressed(KEY_F11))
+        return;
+    SetWindowSize(this->screenWidth, this->screenWidth);
+ 	ToggleFullscreen();
+}
 
-        const char* text = "Your first window!";
-        Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
+void Cevy::GameEngine::update(void) {
+    const char* text = "Your first window!";
+    Vector2 text_size;
+
+    while (!WindowShouldClose()) {
+        Fullscreen();
+        text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
+
         BeginDrawing();
 
         ClearBackground(DARKGRAY);
         DrawText(text, this->screenWidth / 2 - text_size.x / 2, this->screenHeight / 2 - text_size.y / 2, 20, RAYWHITE);
         BeginMode3D(camera);
 
-        DrawGrid(10, 1.0f);
+        DrawGrid(100, 1.0f);
 
         EndMode3D();
-        rlImGuiBegin();
-        bool open = true;
-        ImGui::ShowDemoWindow(&open);
-        DrawFPS(190, 300);
-        rlImGuiEnd();
+        DebugWindow();
         EndDrawing();
     }
 }
--- src/game_engine/game_engine.hpp
+++ src/game_engine/game_engine.hpp
@@ -1,13 +1,20 @@
 #pragma once
 
 #include "raylib.h"
+#include "camera.hpp"
 
 namespace Cevy {
     class GameEngine {
         private:
             int screenWidth = 800;
             int screenHeight = 450;
+            int windowWidth;
+            int windowHeight;
+            bool debugMode = false;
+            Camera camera;
         public:
+            void Fullscreen (void);
+            void DebugWindow (void);
             GameEngine();
             void update(void);
     };
