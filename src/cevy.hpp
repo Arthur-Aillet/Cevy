4ec71fd85a530d06cf1b7b4a6e70ae86c900b312
syborg64 <julian.scott@epitech.eu>
2023-12-07 17:35:49 +0100

    refactor: add root namespace file

---
+++ src/cevy.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2023
+** rtype
+** File description:
+** cevy
+*/
+
+#pragma once
+
+template<typename T>
+using ref = std::reference_wrapper<T>;
+
+/// @brief contains all of the engine bits
+namespace cevy {
+    /// @brief holds the entity components system
+    namespace ecs {};
+
+    /// @brief hold engine mechanics, depends on ecs
+    namespace engine {}; // game-engine ?
+}
+
+// note: here name-spaces are being forwa-rd declared;
+// they will be expanded by their relevant files;
