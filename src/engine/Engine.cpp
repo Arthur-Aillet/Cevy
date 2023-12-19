4a8893ebcd388f9023a9a3572e4d315d02b2a1c8
Auguste Frater <augustefrater@gmail.com>
2023-12-19 13:55:05 +0100

    feat: camera now following with angle

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -82,7 +82,7 @@ void cevy::Engine::build(cevy::ecs::App& app) {
     app.init_component<cevy::Camera>();
     app.init_component<cevy::Position>();
     app.init_component<cevy::Rotation>();
-    app.spawn(cevy::Camera(), cevy::Position(10.0, 10.0, 10.0), cevy::Rotation(0.0, 1.0, 0.0));
+    app.spawn(cevy::Camera(), cevy::Position(10.0, 10.0, 10.0), cevy::Rotation(0.0, 0.6, 1.0));
     // app.add_system<cevy::RenderStage>(control_object);
 }
 
