91252fc671fb7a1f87f9a1ddca149f6e9f757a6e
Axel DENIS <axel.denis@epitech.eu>
2024-01-03 19:46:14 +0100

    feat(windows): adapted dev to windows

--- src/ecs/Entity.cpp
+++ src/ecs/Entity.cpp
@@ -13,4 +13,4 @@ Entity::Entity(std::size_t new_id) : _id(new_id) {}
 
 Entity::operator std::size_t &() { return this->_id; }
 
-Entity::operator std::size_t() const { return this->_id; }
+Entity::operator std::size_t() const {return this->_id; }
