85d4c17b63453c744702f743e6b6e8427cead77c
Arthur-Aillet <arthur.aillet@epitech.eu>
2024-01-11 19:11:55 +0100

    doc: Stage file documented

--- src/ecs/Stage.hpp
+++ src/ecs/Stage.hpp
@@ -2,7 +2,7 @@
 ** Agartha-Software, 2023
 ** Cevy
 ** File description:
-** Scheduler
+** Stage
 */
 
 #pragma once
@@ -11,8 +11,38 @@
 #include <type_traits>
 
 namespace cevy::ecs {
+/**
+ * @brief Base class for Stage used as Id for type checking
+ */
 struct IsStage {};
 
+/**
+ * @brief Base class for any Stage, is made to be user generated
+ *
+ * If the Stage-Ordering don't find a place to place the stage it will be pushed at the end of the
+ * queue
+ *
+ * Some shorthands exists to create a stage after an other:
+ * - at_start
+ * - after
+ * - before
+ *
+ * '''
+ * class Startup : public at_start {};
+ *
+ * class PreStartup : public before<Startup> {};
+ * class PostStartup : public after<Startup> {};
+ *
+ * @tparam Before Previous Stage
+ *
+ * Other Stage or std::nullopt_t (default)
+ * @tparam After Next Stage
+ *
+ * Other Stage or std::nullopt_t (default)
+ * @tparam Repeat Is it run once at app.run() or each cycle (Update vs Startup)
+ *
+ * std::false_type or std::true_type (default)
+ */
 template <typename Before = std::nullopt_t, typename After = std::nullopt_t,
           typename Repeat = std::true_type>
 class Stage : public IsStage {
@@ -28,11 +58,35 @@ class Stage : public IsStage {
   using after = Stage<std::nullopt_t, T, typename T::is_repeat>;
 };
 
-template <typename T>
-using before = Stage<>::before<T>;
-template <typename T>
-using after = Stage<>::after<T>;
+/**
+ * @brief Shorthand to create a stage before an other
+ *
+ * '''
+ * class Startup : public at_start {};
+ * class PreStartup : public before<Startup> {};
+ * '''
+ *
+ * @tparam LastStage
+ */
+template <typename LastStage>
+using before = Stage<>::before<LastStage>;
 
+/**
+ * @brief Shorthand to create a stage after an other
+ *
+ * '''
+ * class Startup : public at_start {};
+ * class PostStartup : public after<Startup> {};
+ * '''
+ *
+ * @tparam NextStage
+ */
+template <typename NextStage>
+using after = Stage<>::after<NextStage>;
+
+/**
+ * @brief Default stage constructor
+ */
 using at_start = Stage<std::nullopt_t, std::nullopt_t, std::false_type>;
 
 class Startup : public at_start {};
@@ -45,7 +99,16 @@ class Update : public after<First> {};
 class PreUpdate : public before<Update> {};
 class PostUpdate : public after<Update> {};
 
+/**
+ * @brief Run between each state transitions
+ * @bug NOT IMPLEMENTED YET
+ */
 class StateTransition : public before<Update> {};
+
+/**
+ * @brief Run a fixed number of time each time unit
+ * @bug NOT IMPLEMENTED YET
+ */
 class RunFixedUpdateLoop : public before<Update> {};
 
 class Last : public after<PostUpdate> {};
